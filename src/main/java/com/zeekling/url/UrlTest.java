d67901837a55878cae16a3baec0de4da946480ef
zeekling <lingzhaohui@zeekling.cn>
2021-04-30 00:51:00 +0800

    删除配置文件

--- src/main/java/com/zeekling/url/UrlTest.java
+++ src/main/java/com/zeekling/url/UrlTest.java
@@ -6,10 +6,10 @@ import java.net.URL;
 public class UrlTest {
 
     public static void main(String[] args) throws MalformedURLException {
-        URL u1 =new URL("http://www.111.com");
+        URL u1 = new URL("http://www.111.com");
         System.out.println(u1);
-        URL u2 =new URL("https://www.baidu.com");
+        URL u2 = new URL("https://www.baidu.com");
         System.out.println(u2);
     }
-    
+
 }
